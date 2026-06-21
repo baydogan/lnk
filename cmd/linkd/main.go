eb0350eb3c9139bd3dd9358e5bb3aed9bae86c23
baydogan <aydoganbugra93@gmail.com>
2026-06-21 19:43:09 +0300

    Health, router and server files created

--- cmd/linkd/main.go
+++ cmd/linkd/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/baydogan/lnk/internal/server"
+)
+
+func main() {
+
+	router := server.NewRouter()
+
+	srv := &http.Server{
+		Addr:    ":" + "8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		}
+	}()
+}
